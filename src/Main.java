3a04588bb800c5ca18a1918a73d065db55b47fb8
Bart-PPG <Bartek.nitek@onet.pl>
2024-09-04 13:40:50 +0200

    losowanie liczby rzecywistej

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Witaj na lekcji");
+
+        double wylosowana = Math.random()*10+1;
+        System.out.println(wylosowana);
     }
 }
